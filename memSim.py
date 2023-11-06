bcf3a87ef8778439afbd71dabb8bcb21d8fd73b2
Ethan Wagner <ewagne02@unix4.csc.calpoly.edu>
2023-11-05 18:59:02 -0800

    algorithm pseudo

--- memSim.py
+++ memSim.py
@@ -1,8 +1,15 @@
 from typing import List, Dict
+from queue import Queue
 import argparse
 
+PAGE_TABLE_SIZE = 2**8
+PAGE_SIZE = 2**8 # bytes
+FRAME_SIZE = PAGE_SIZE
+DISK_SIZE = PAGE_TABLE_SIZE * PAGE_SIZE # bytes
+TLB_SIZE = 16
+
 class TLB: # pretty much just a cache, FIFO
-    def __init__(self, size: int=16):
+    def __init__(self, size: int=TLB_SIZE):
         self.size = size
         self.tlb = [None] * size
         self.evictionIndex = 0 # need to keep track of what we want to evict
@@ -15,21 +22,13 @@ class TLB: # pretty much just a cache, FIFO
             self.tlb[self.evictionIndex] = (pageNumber, frameNumber)
             self.evictionIndex = (self.evictionIndex + 1) % self.size
 
-class PageTableFIFO: # include a loaded bit for each entry
-    def __init__(self, size: int=2**8, algorithm: str="fifo"):
-        self.size = size
-        self.pageTable = {}
-class PageTableOPT: # include a loaded bit for each entry
-    def __init__(self, size: int=2**8, algorithm: str="fifo"):
-        self.size = size
-        self.pageTable = {}
-class PageTablLRU: # include a loaded bit for each entry
-    def __init__(self, size: int=2**8, algorithm: str="fifo"):
+class PageTable: # include a loaded bit for each entry
+    def __init__(self, size: int=PAGE_TABLE_SIZE):
         self.size = size
         self.pageTable = {}
 
 class Disk: # AKA Backing Store
-    def __init__(self, size: int):
+    def __init__(self, size: int=DISK_SIZE):
         self.size = size
         self.disk = {}
 
@@ -38,11 +37,14 @@ class RAM: # AKA Physical Memory
         self.size = size
         self.ram = {}
 
-
-    
+class PTEntry: # page table entry has frame number and loaded bit, the page number is the index of the entry
+    def __init__(self, frame: int):
+        self.frame = None
+        self.loaded = 0    
 
 # Main will do all the simulation logic, prob should use helper functions.
 def main():
+
     parser = argparse.ArgumentParser(description="Memory Simulator")
     # Required argument for reference-sequence-file.txt
     parser.add_argument("reference_sequence_file", type=str,
@@ -55,8 +57,82 @@ def main():
                         help="Page Replacement Algorithm. Choices are 'fifo', 'lru', or 'opt'. Default is 'fifo'.")
     args = parser.parse_args()
     
+    # get frames from args
+    frames = args.frames
 
+    #initialize page table
+    pt = PageTable()
+    #initialize tlb
+    tlb = TLB()
+    #initialize ram
+    memory = RAM(size=frames)
+    #initialize disk
+    disk = Disk()
 
+    # open file to read
+    f = open(args.reference_sequence_file, 'r')
+
+    if args.pra == "lru": # least recently used
+        # lru counters
+        lru_counter = [0]*256
+        fifo = Queue(maxsize=frames) # to break ties, but could also just use the first value that pops out of min?
+        # while loop that goes through the addresses
+            # increment counter for this page
+        # check tlb, check loaded bit
+
+        #check page table, check loaded bit
+            # if hit go to memory
+            # get value
+
+            #else 
+                # page swap
+                    # check for free frames
+                    # get least from lru_counter that is in memory currently
+                    # remove from memory
+                    # add disk page into memory
+                # restart instruction
+    
+    elif args.pra == "opt": # optimal replacement
+        fifo = Queue(maxsize=frames) # to break ties, but could also just use the first value that pops out of min?
+        # read all of addresses.txt
+            # do conversions and add all instances of each page
+            # store in array
+        # while loop that goes through the addresses
+            # decement counter for this page
+        # check tlb, check loaded bit
+
+        #check page table, check loaded bit
+            # if hit go to memory
+            # get value
+
+            #else 
+                # page swap
+                    # check for free frames
+                    # get least from lru_counter that is in memory currently
+                    # remove from memory
+                    # add disk page into memory
+                # restart instruction
+        pass
+    
+    else:   # first in first out
+        # fifo queue
+        fifo = Queue(maxsize=frames)
+
+        # while loop that goes through the addresses
+
+        # check tlb, check loaded bit
+
+        #check page table, check loaded bit
+            # if hit go to memory
+            # get value
+
+            #else 
+                # page swap
+                    # pop queue
+                    # remove from memory
+                    # add disk page into memory
+                # restart instruction
+    f.close()
 
 if __name__ == "__main__":
     main()
