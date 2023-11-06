6807f49d9f9245d66fa59affafcc04490ea3294a
nickambrose7 <nickambrose7@gmail.com>
2023-11-05 18:44:43 -0800

    Disk object finished

--- memSim.py
+++ memSim.py
@@ -15,23 +15,27 @@ class TLB: # pretty much just a cache, FIFO
             self.tlb[self.evictionIndex] = (pageNumber, frameNumber)
             self.evictionIndex = (self.evictionIndex + 1) % self.size
 
-class PageTableFIFO: # include a loaded bit for each entry
-    def __init__(self, size: int=2**8, algorithm: str="fifo"):
-        self.size = size
-        self.pageTable = {}
-class PageTableOPT: # include a loaded bit for each entry
-    def __init__(self, size: int=2**8, algorithm: str="fifo"):
-        self.size = size
-        self.pageTable = {}
-class PageTablLRU: # include a loaded bit for each entry
+class PageTable: # include a loaded bit for each entry
     def __init__(self, size: int=2**8, algorithm: str="fifo"):
         self.size = size
         self.pageTable = {}
 
-class Disk: # AKA Backing Store
-    def __init__(self, size: int):
-        self.size = size
-        self.disk = {}
+class Disk:  # AKA Backing Store
+    """
+    The backing store can be represented by a dictionary of size 256
+    where the key is the page number and the value is all 256 bytes in the frame.
+    We can make the value an array of size 256, making it easy to offset into the frame.
+    """
+    def __init__(self, filename: str):
+        self.disk = self.file_to_frames(filename)
+
+    @staticmethod
+    def file_to_frames(filename):
+        frames = {}
+        with open(filename, 'rb') as f:
+            for i in range(256):
+                frames[i] = list(f.read(256))
+        return frames
 
 class RAM: # AKA Physical Memory
     def __init__(self, size: int):
@@ -55,6 +59,24 @@ def main():
                         help="Page Replacement Algorithm. Choices are 'fifo', 'lru', or 'opt'. Default is 'fifo'.")
     args = parser.parse_args()
     
+
+    # Read in the reference sequence file
+
+    # Read in the entire backing store file into an easy to access data structure
+    disk = Disk("BACKING_STORE.bin")
+
+    #For each logical address in the reference sequence file:
+
+    # 1. Determine the page number and offset
+    # 2. Check the TLB for the page number
+    # 3. If the page number is in the TLB, get the frame number
+    # 4. If the page number is not in the TLB, check the page table
+    # 5. If the page number is in the page table, get the frame number
+    # 6. If the page number is not in the page table, get a free frame from RAM
+    # 7. If there are no free frames, use the page replacement algorithm to select a victim frame
+    # 8. Update the page table and TLB, getting the correct page from the disk if necessary.
+    
+
 
 
 
